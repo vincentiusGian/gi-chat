fece9613dafa9218f52d34b4454aa76c775fd3de
vincentiusgian <gianjunius54@gmail.com>
2023-09-28 22:46:08 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css';
+import io from 'socket.io-client';
+import { useState } from 'react';
+import Chat from './Chat';
+
+const socket = io.connect("http://localhost:3001");
+
+
+function App() {
+
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+  
+  return (
+    <div className='App flex justify-center m-10'>
+      { !showChat ? (
+      <div className="card w-96 bg-base-100 shadow-xl ">
+        <figure><img src="" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">G-CHAT</h2>
+          <p>My first chatting app!! </p>
+          <input type="text" placeholder="Your name" className="input input-bordered input-info w-full max-w-xs" onChange={(event) => {
+              setUsername(event.target.value);
+            }}/>
+          <input type="text" placeholder="Room ID" className="input input-bordered input-info w-full max-w-xs" onChange={(event) => {
+              setRoom(event.target.value);
+            }}/>
+          <div className="card-actions justify-end">
+            <button className="btn btn-neutral" onClick={joinRoom}>Chat Now!</button>
+          </div>
+        </div>
+      </div>
+  ) : (
+    <Chat socket={socket} username={username} room={room} />
+  )}
+</div>
+); 
+}
+
+export default App;
---
+++ client/src/Chat.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    }); 
+    return () => socket.off("receive_message"); 
+  }, [socket]);
+
+  return (
+    <div className="chat-window mt-1">
+      <div className="chat-header">
+        <p>ROOM {room}</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input className="input input-bordered input-info w-full max-w-xs"
+          type="text"
+          value={currentMessage}
+          placeholder="Type here..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
