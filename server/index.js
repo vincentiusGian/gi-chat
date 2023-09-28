abdff0438bc92dcc2466131058715da44959713a
vincentiusGian <126053267+vincentiusGian@users.noreply.github.com>
2023-09-28 23:08:01 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
 
     cors: {
         
-        origin: "http://localhost:3000",
+        origin: "https://gi-chat-apps.vercel.app/",
         methods: ["GET", "POST"],
 
     },
