844e7d2f98e09c905e8565604101eb8f01328eef
vincentiusGian <126053267+vincentiusGian@users.noreply.github.com>
2023-09-28 23:28:40 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const http = require("http");
+const https = require("https");
 const cors = require('cors');
 const { Server } = require("socket.io");
 app.use(cors());
